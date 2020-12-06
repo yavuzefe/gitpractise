558b6a7f33663a9973fc02b46bb15e5a59db6b0c
yavuzefe <yavuzefe@gmail.com>
2020-12-06 09:06:37 +0100

    main branchte ilk uc satir degistirildi

--- conflict.js
+++ conflict.js
@@ -1,1 +1,3 @@
-// main branch'te eklendi
+// main branch'te eklendi, mainde tekrar degistirildi.
+let main = "main branch'te eklendi";
+let farkliSatir = "mainde farkli bir satira bir degisken daha eklendi";
